92ba2b96442945bad922ecf7ad7b57d255eb8000
Edwin Gutierrez <edwinj.gutierrezm@gmail.com>
2020-08-14 17:39:14 -0700

    db

--- b2bmayoreoapi/settings/prod.py
+++ b2bmayoreoapi/settings/prod.py
@@ -1,13 +1,25 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bluesage.tk']
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'b2bmayoreodb',
+        'USER': 'b2bmayoreo',
+        'PASSWORD': 'Solecismo1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 STATIC_URL = '/static/'
