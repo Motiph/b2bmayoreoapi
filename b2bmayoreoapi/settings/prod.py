e866c52a15b04f1ddb5b2a90f81e0368c18f5493
Edwin Gutierrez <edwinj.gutierrezm@gmail.com>
2020-08-14 18:50:33 -0700

    python anywhere

--- b2bmayoreoapi/settings/prod.py
+++ b2bmayoreoapi/settings/prod.py
@@ -1,25 +1,28 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ['bluesage.tk']
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'b2bmayoreodb',
-        'USER': 'b2bmayoreo',
-        'PASSWORD': 'Solecismo1',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'b2bmayoreodb',
+#         'USER': 'b2bmayoreo',
+#         'PASSWORD': 'Solecismo1',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
