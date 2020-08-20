c55c757bb8ed9611d200af324feb17dd9dc36836
Edwin Gutierrez <edwinj.gutierrezm@gmail.com>
2020-08-19 18:39:33 -0700

    se cambio el call a pacesetter de nuxt a una vista de django

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+import requests
 
-# Create your views here.
-# class StoreZoneViewSet(DefaultViewSetMixin, ModelViewSetMixin):
-#     queryset = StoreZone.objects.all()
-#     serializer_class = StoreZoneSerializer
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+
+
+
+class PaceSetter (APIView):
+    def post(self, request, format=None):
+        url = 'http://189.223.128.30:8081/'
+        headers = {'Content-Type': 'text/xml'}
+        xml = request.data['xml']
+        r = requests.post(url, xml, headers=headers)
+
+        print(r.text)
+
+        return Response(r.text)
